be8971f41c634bc73a09cacff2ad97d76085c5ff
Arabinda Mohanty <arabindamohanty41@gmail.com>
2024-10-20 12:30:03 +0530

    added/updated/delted files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,5 +3,8 @@ package pages;
 public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("hello login page");
+	String un="admin";
+	String pwd ="admin";
+	System.out.println("logged in....");
 }
 }
