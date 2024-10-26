e8b0fd89377b31aa587d36f20cc3a6c82ab175a5
Arabinda Mohanty <arabindamohanty41@gmail.com>
2024-10-26 16:08:16 +0530

    updated cart feature

--- src/tests/UpdatedCart.java
+++ src/tests/UpdatedCart.java
@@ -1,7 +1,8 @@
 package tests;
 
 public class UpdatedCart {
-public void UICart() {
-	System.out.println("ui cart");
-}
+	public void UICart() {
+		System.out.println("ui cart");
+		System.out.println("cart page feature");
+	}
 }
